f1731542f93ca8444923a40cbf073e9634a5ece3
Gilchrist32 <32stephencurry@gmail.com>
2021-06-28 12:42:57 +0800

    UPDATED SCRIPT

--- js/main.js
+++ js/main.js
@@ -253,3 +253,14 @@ $(document).ready(function(){
         type: "iframe"
     })
 })
+
+// Contact //
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxfFqLyse7vWJsa7hukxfppDWv5jPo-QyLYvzfSUZo8zchRa2kS6QzGDbZCQu4Wl4qGVQ/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .catch(error => console.error('Error!', error.message))
+            })
